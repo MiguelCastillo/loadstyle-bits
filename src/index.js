c29519e583d104a15514da31c90e380ad77d2e31
Miguel Castillo <manchagnu@gmail.com>
2016-06-15 00:50:57 -0400

    added support for specifying css as links for better livereload support

--- src/index.js
+++ src/index.js
@@ -6,17 +6,33 @@ function attachToDOM(source) {
   head.appendChild(style);
 }
 
-function loadStyle(moduleMeta /*, options*/) {
-  attachToDOM(moduleMeta.source);
+function loadAsLink(path) {
+  var head = document.getElementsByTagName('head')[0];
+  var link = document.createElement('link');
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('href', path);
+  head.appendChild(link);
+}
+
+function loadStyle(data, options) {
+  options = options || {};
+
+  if (options.embedded) {
+    attachToDOM(data.source);
+  }
+  else {
+    loadAsLink(data.path);
+  }
 
   return {
-    exports: moduleMeta.source
+    exports: data.source
   };
 }
 
 loadStyle.configure = function(options) {
-  return function(moduleMeta) {
-    loadStyle(moduleMeta, options);
+  return function(data) {
+    loadStyle(data, options);
   };
 };
 
